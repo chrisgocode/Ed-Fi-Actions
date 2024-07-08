d562d382ba0e4129729a34fbf2072bbadb7e34c0
Christian <chrisgo@bu.edu>
2024-07-08 09:35:31 -0500

    Resolved comments:
    - Reformated with flake8 + black
    - Added debug and error messages
    - modified Dockerfile to use 'poetry install --only main'
    - added testing.md

--- action-allowedlist/action_allowedlist/__main__.py
+++ action-allowedlist/action_allowedlist/__main__.py
@@ -1,12 +1,13 @@
-# SPDX-License-Identifier: Apache-2.0
-# Licensed to the Ed-Fi Alliance under one or more agreements.
-# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
-# See the LICENSE and NOTICES files in the project root for more information.
+# SPDX-License-Identifier: Apache-2.0 Licensed to the Ed-Fi Alliance under one
+# or more agreements. The Ed-Fi Alliance licenses this file to you under the
+# Apache License, Version 2.0. See the LICENSE and NOTICES files in the project
+# root for more information.
 
 from actions_parser import get_all_used_actions, invoke_validate_actions
 from pathlib import Path
 import sys
 from os.path import abspath
+
 
 def main(workflow_directory: Path, approved_path: Path):
     print(f"Repository path to scan: {workflow_directory}")
@@ -20,6 +21,7 @@ def main(workflow_directory: Path, approved_path: Path):
         sys.exit(1)
     else:
         sys.exit(0)
+
 
 if __name__ == "__main__":
     workflow_directory = Path(abspath(sys.argv[1]))
--- action-allowedlist/action_allowedlist/actions_parser.py
+++ action-allowedlist/action_allowedlist/actions_parser.py
@@ -9,16 +9,17 @@ from pathlib import Path
 
 
 def load_json_file(filepath):
-    with open(filepath, 'r') as file:
+    with open(filepath, "r") as file:
         content = file.read()
         return json.loads(content)
 
+
 def load_yaml_file(filepath):
-    with open(filepath, 'r') as file:
+    with open(filepath, "r") as file:
         return yaml.safe_load(file)
 
+
 def get_actions_from_file(workflow, workflow_file_name):
-    #print(workflow_file_name)
     parsed_yaml = yaml.safe_load(workflow)
     actions = []
 
@@ -27,34 +28,39 @@ def get_actions_from_file(workflow, workflow_file_name):
         steps = job.get("steps", [])
         for step in steps:
             uses = step.get("uses")
-            if (uses != None):
+            if uses is not None:
                 parts = uses.split("@")
                 if len(parts) == 2:
                     action_link, action_version = parts
-                    actions.append({
-                        "actionLink": action_link,
-                        "actionVersion": action_version,
-                        "workflowFileName": workflow_file_name
-                    })
+                    actions.append(
+                        {
+                            "actionLink": action_link,
+                            "actionVersion": action_version,
+                            "workflowFileName": workflow_file_name,
+                        }
+                    )
                     print(f"   Found action used: [{uses}]")
 
     return actions
+
 
 def get_all_used_actions(workflow_dir: Path):
     print("Loading Actions YAML files")
     if (workflow_dir / ".github/workflows").exists():
         workflow_files = list((workflow_dir / ".github/workflows").glob("*.yml"))
     else:
-        workflow_files = list((workflow_dir / "testing-repo/.github/workflows").glob("*.yml"))
+        workflow_files = list(
+            (workflow_dir / "testing-repo/.github/workflows").glob("*.yml")
+        )
     if not workflow_files:
         print("Could not find workflow files in the specified directory")
         return []
 
     print(f"Found [{len(workflow_files)}] files in the workflows directory")
-    #print(workflow_files)
+
     actions_in_repo = []
+
     for workflow_file in workflow_files:
-        #print(workflow_file)
         try:
             workflow_content = workflow_file.read_text()
             actions = get_actions_from_file(workflow_content, workflow_file.name)
@@ -63,6 +69,7 @@ def get_all_used_actions(workflow_dir: Path):
             print(f"Error occurred while reading {workflow_file}: {e}")
 
     return actions_in_repo
+
 
 def invoke_validate_actions(approved_path, actions_configuration):
     print("Checking if used actions are approved")
@@ -75,39 +82,51 @@ def invoke_validate_actions(approved_path, actions_configuration):
     approved_outputs = []
     found = False
 
-
     for action in actions_configuration:
-        # print(f"Processing {action['actionLink']} version {action['actionVersion']}") Use for debugging
-
-        approved_action_versions = [a for a in approved if a['actionLink'] == action['actionLink']]
+        print(
+            f"::debug::Processing {action['actionLink']} version {action['actionVersion']}"
+        )
+        approved_action_versions = [
+            a for a in approved if a["actionLink"] == action["actionLink"]
+        ]
         if approved_action_versions:
-            # print(f"Approved Versions for {action['actionLink']}: {[a['actionVersion'] for a in approved_action_versions]}") Use for debugging
-            pass
+            print(
+                f"::debug::Approved Versions for {action['actionLink']}: {[a['actionVersion']
+                                                                           for a in approved_action_versions]}"
+            )
         else:
-            # print(f"No Approved versions for {action['actionLink']} were found.") Use for debugging
+            print(
+                f"::debug::No Approved versions for {action['actionLink']} were found."
+            )
             pass
 
-        approved_output = [a for a in approved_action_versions if a['actionVersion'] == action['actionVersion']]
+        approved_output = [
+            a
+            for a in approved_action_versions
+            if a["actionVersion"] == action["actionVersion"]
+        ]
 
         if approved_output:
-            # print(f"Output versions approved: {approved_output}") Use for debugging
+            print(f"::debug::Output versions approved: {approved_output}")
             approved_outputs.append(approved_output[0])
             num_approved += 1
 
             # Look for deprecation
-            if approved_output[0].get('deprecated', False):
+            if approved_output[0].get("deprecated", False):
                 print(f"Using a deprecated version of {action['actionLink']}")
                 num_deprecated += 1
                 found = True
         else:
-            # print(f"Output versions not approved: {action['actionLink']} version {action['actionVersion']}") Use for debugging
-            unapproved_outputs.append(f"{action['actionLink']} {action['actionVersion']}")
+            print(f"::debug::Output versions not approved: {action['actionLink']} version {action['actionVersion']}")
+            unapproved_outputs.append(
+                f"{action['actionLink']} {action['actionVersion']}"
+            )
             num_denied += 1
             found = True
 
     if num_denied > 0:
         e = f"The following {num_denied} actions/versions were denied: {', '.join(unapproved_outputs)}"
-        print(e)
+        print(f"::error file=actions_parser.py,title=Denied Actions::{e}")
         return found
     else:
         print(f"All {num_approved} actions/versions are approved.")
